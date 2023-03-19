a03f193e4584db1cb8a6c3e2783fb6b9179f520d
Simcoe Alkadhi <simcoea@gmail.com>
2023-03-19 05:59:33 -0400

    yarn prettier

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -7,62 +7,62 @@ import { Wallet, utils } from 'zksync-web3';
 import * as ethers from 'ethers';
 
 export default async function (hre: HardhatRuntimeEnvironment) {
-    console.log('Deploying Album smart contract.');
-    const wallet = new Wallet(process.env.PRIVATE_KEY || '');
-    const deployer = new Deployer(hre, wallet);
+  console.log('Deploying Album smart contract.');
+  const wallet = new Wallet(process.env.PRIVATE_KEY || '');
+  const deployer = new Deployer(hre, wallet);
 
-    const albumArtifact = await deployer.loadArtifact('Album');
-    const albumArgs = ['Album', 'ALB', 10_000];
-    const albumDeploymentFee = await deployer.estimateDeployFee(albumArtifact, albumArgs);
+  const albumArtifact = await deployer.loadArtifact('Album');
+  const albumArgs = ['Album', 'ALB', 10_000];
+  const albumDeploymentFee = await deployer.estimateDeployFee(albumArtifact, albumArgs);
 
-    const albumDepositHandle = await deployer.zkWallet.deposit({
-        to: deployer.zkWallet.address,
-        token: utils.ETH_ADDRESS,
-        amount: albumDeploymentFee,
-    });
-    
-    await albumDepositHandle.wait();
+  const albumDepositHandle = await deployer.zkWallet.deposit({
+    to: deployer.zkWallet.address,
+    token: utils.ETH_ADDRESS,
+    amount: albumDeploymentFee,
+  });
 
-    const parsedAlbumFee = ethers.utils.formatEther(albumDeploymentFee.toString());
-    console.log(`The deployment is estimated to cost ${parsedAlbumFee} ETH`);
+  await albumDepositHandle.wait();
 
-    const album = <Album>await deployer.deploy(albumArtifact, albumArgs);
+  const parsedAlbumFee = ethers.utils.formatEther(albumDeploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedAlbumFee} ETH`);
 
-    await album.deployed();
-    console.log(`Album smart contract deployed to ${album.address}`);
+  const album = <Album>await deployer.deploy(albumArtifact, albumArgs);
 
-    console.log('Deploying Song smart contract.');
-    const songArtifact = await deployer.loadArtifact('Song');
-    const songArgs = ['Song', 'SNG'];
-    const songDeploymentFee = await deployer.estimateDeployFee(songArtifact, songArgs);
-    const songDepositHandle = await deployer.zkWallet.deposit({
-        to: deployer.zkWallet.address,
-        token: utils.ETH_ADDRESS,
-        amount: albumDeploymentFee
-    });
+  await album.deployed();
+  console.log(`Album smart contract deployed to ${album.address}`);
 
-    await songDepositHandle.wait();
+  console.log('Deploying Song smart contract.');
+  const songArtifact = await deployer.loadArtifact('Song');
+  const songArgs = ['Song', 'SNG'];
+  const songDeploymentFee = await deployer.estimateDeployFee(songArtifact, songArgs);
+  const songDepositHandle = await deployer.zkWallet.deposit({
+    to: deployer.zkWallet.address,
+    token: utils.ETH_ADDRESS,
+    amount: albumDeploymentFee,
+  });
 
-    const parsedSongFee = ethers.utils.formatEther(songDeploymentFee.toString());
-    console.log(`The deployment is estimated to cost ${parsedSongFee} ETH`);
+  await songDepositHandle.wait();
 
-    const song = <Song>await deployer.deploy(songArtifact, songArgs);
+  const parsedSongFee = ethers.utils.formatEther(songDeploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedSongFee} ETH`);
 
-    await song.deployed();
-    console.log(`Song smart contract deployed to ${song.address}.`);
+  const song = <Song>await deployer.deploy(songArtifact, songArgs);
 
-    await delay(20000);
-    console.log('Verifying smart contracts.');
+  await song.deployed();
+  console.log(`Song smart contract deployed to ${song.address}.`);
 
-    await run(`verify:verify`, {
-        address: album.address,
-        constructorArguments: albumArgs,
-        contract: 'contracts/Album.sol:Album',
-    });
+  await delay(20000);
+  console.log('Verifying smart contracts.');
 
-    await run(`verify:verify`, {
-        address: song.address,
-        constructorArguments: songArgs,
-        contract: 'contracts/Song.sol:Song',
-    });
+  await run(`verify:verify`, {
+    address: album.address,
+    constructorArguments: albumArgs,
+    contract: 'contracts/Album.sol:Album',
+  });
+
+  await run(`verify:verify`, {
+    address: song.address,
+    constructorArguments: songArgs,
+    contract: 'contracts/Song.sol:Song',
+  });
 }
